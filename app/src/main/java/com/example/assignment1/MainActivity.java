a7bd2b65e852a3e1c246bd842b4e88b803cd90ab
trondhth <trondhth@stud.ntnu.no>
2019-01-20 17:08:47 +0100

    MainActivity GUI mostly done

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.assignment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        Button button = findViewById(R.id.app_logo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transferActivity();
+            }
+        });
+        */
+    }
+
+    public void transferActivity() {
+        Intent intent = new Intent(this, AnotherActivity.class);
+        startActivity(intent);
     }
 }
