74281c07ec45bc4721a47b6fc9272f8341c32956
trondhth <trondhth@stud.ntnu.no>
2019-01-20 18:05:05 +0100

    Java code for MainActivity (mostly) complete

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +17,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        Button button = findViewById(R.id.app_logo);
-        button.setOnClickListener(new View.OnClickListener() {
+        // Calling function to initialize account balance.
+        init_balance();
+
+        // Creating button linking to transactions page.
+        Button button_transactions = findViewById(R.id.btn_transactions);
+        button_transactions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transactionActivity();
+            }
+        });
+
+        // Creating button linking to transfer page.
+        Button button_transfer = findViewById(R.id.btn_transfer);
+        button_transfer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 transferActivity();
             }
         });
-        */
+
     }
 
-    public void transferActivity() {
-        Intent intent = new Intent(this, AnotherActivity.class);
+
+    // Starting activity 'TransactionActivity' when button btn_transactions is pressed.
+    public void transactionActivity() {
+        Intent intent = new Intent(this, TransactionsActivity.class);
         startActivity(intent);
+    }
+
+    // Starting activity 'TransferActivity' when button btn_transfer is pressed.
+    public void transferActivity() {
+        Intent intent = new Intent(this, TransferActivity.class);
+        startActivity(intent);
+    }
+
+    // Initializing the account balance, range [90, 110].
+    public void init_balance() {
+        Random r = new Random();
+        int balance = r.ints(1, 90, 111).findFirst().getAsInt();
+        //int balance = ThreadLocalRandom.current().nextInt(90, 110+1);
+        final TextView balance_text = (TextView) findViewById(R.id.lbl_balance);
+        balance_text.setText(String.valueOf(balance));
     }
 }
--- app/src/main/java/com/example/assignment1/TransactionsActivity.java
+++ app/src/main/java/com/example/assignment1/TransactionsActivity.java
@@ -3,13 +3,11 @@ package com.example.assignment1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AnotherActivity extends AppCompatActivity {
+public class TransactionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_another);
-
-        System.out.println("test 1234 hello world\n");
+        setContentView(R.layout.activity_transactions);
     }
 }
